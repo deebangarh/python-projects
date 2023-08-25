df0555037b2bef93610f1c96203a2ca1511d39b3
Deepika Bangarh <deepikabangarh@yahoo.com>
2023-08-25 12:27:01 +0100

    game

--- app.py
+++ app.py
@@ -4,3 +4,23 @@ while x <= 5:
     x+= 1
 
 print("finito")
+
+# ////
+
+word = "hooray"
+guess_word = ""
+number_of_guesses = 0
+max_guesses = 4
+out_of_guesses = False
+
+while guess_word != word and not(out_of_guesses) :
+    if number_of_guesses < max_guesses:
+     guess_word = input("what is your guess: ")
+     number_of_guesses += 1
+    else:
+       out_of_guesses = True
+if out_of_guesses:
+ print("you are out of guesses")
+else:
+  print("you win")
+
